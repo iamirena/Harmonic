ac91af87493471d48b4354d607168f686d700ade
irena <irenapinc@gmail.com>
2022-09-27 11:04:56 +0300

    гг

--- src/lab1/task7/Factor.java
+++ src/lab1/task7/Factor.java
@@ -1,5 +1,16 @@
 package lab1.task7;
-
+import java.util.Scanner;
 public class Factor {
-
+    public static int Factorial(int a){
+        int b = 1;
+        while (a!=0){
+            b = b*a;
+            a = a - 1;
+        }
+        return b;
+    }
+    public static void main(String[] args){
+        Scanner x = new Scanner(System.in);
+        System.out.println(Factorial(x.nextInt()));
+    }
 }
