0cb05e2f7429c573cfd294581b6c168c588badbf
unknown <ivc1-5@VLAN-25-15-086.rd.mirea.ru>
2022-09-27 10:15:30 +0300

    Initial commit

---
+++ src/lab1/task6/Harmonic.java
@@ -0,0 +1,15 @@
+package lab1.task6;
+import java.util.Scanner;
+public class Harmonic {
+    public static void main(String[] args){
+        int i;
+        double sum = 0;
+        double p = 0;
+        for (i = 1; i <= 10; i++){
+            p = 1.0/i;
+            sum += p;
+            System.out.printf("\n%.2f",p);
+        }
+        System.out.printf("\n%.2f",sum);
+    }
+}
